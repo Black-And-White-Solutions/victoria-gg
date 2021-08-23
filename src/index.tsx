c3fa0690dacff29417d5ebba6056d39952720d6d
Luis Gabriel Ramos Robles <luisgabrielramosrobles@gmail.com>
2021-08-23 19:43:57 -0400

    Set CSS-in-JS and cleared create-react-app content

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createGlobalStyle } from 'styled-components';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const GlobalStyles = createGlobalStyle`
+	* {
+	  box-sizing: border-box;
+	  padding: 0;
+		margin: 0;
+	}
+
+		body {
+		background-color: rgb(0, 0, 0);
+	  color: rgb(0, 0, 0);
+		font-family: 'Alegreya Sans', 'Lato', sans-serif, -apple-system, BlinkMacSystemFont,
+	    'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans',
+	    'Droid Sans', 'Helvetica Neue';
+	  -webkit-font-smoothing: antialiased;
+	  -moz-osx-font-smoothing: grayscale;
+	  font-style: normal;
+	  font-weight: normal;
+	  word-wrap: normal;
+	}
+
+	p {
+		text-align: justify;
+	  text-justify: inter-word;
+	}
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
