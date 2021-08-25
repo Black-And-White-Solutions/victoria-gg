5e7e9efd05b959738bbd9f90a7014c1921d5dc52
Luis Gabriel Ramos Robles <luisgabrielramosrobles@gmail.com>
2021-08-25 12:00:35 -0400

    Created main router component

--- src/components/PagesRouter.tsx
+++ src/components/PagesRouter.tsx
@@ -1,11 +1,19 @@
 import React, { VoidFunctionComponent } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home';
+import About from '../pages/About';
+import HowItWorks from '../pages/HowItWorks';
+import Catalogue from '../pages/Catalogue';
+import SingIn from '../pages/SingIn';
 
 const PagesRouter: VoidFunctionComponent = () => {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/howItWorks" component={HowItWorks} />
+      <Route path="/catalogue" component={Catalogue} />
+      <Route path="/singIn" component={SingIn} />
     </Switch>
   );
 };
---
+++ src/pages/About.tsx
@@ -0,0 +1,7 @@
+import React, { VoidFunctionComponent } from 'react';
+
+const About: VoidFunctionComponent = () => {
+  return <h1>About</h1>;
+};
+
+export default About;
