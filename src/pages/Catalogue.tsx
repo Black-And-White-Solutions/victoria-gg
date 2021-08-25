7ac99ec62ef0e4543ff0df2f258740e2311680f7
Luis Gabriel Ramos Robles <luisgabrielramosrobles@gmail.com>
2021-08-25 08:59:21 -0400

    Template pages created

---
+++ src/pages/Catalogue.tsx
@@ -0,0 +1,7 @@
+import React, { VoidFunctionComponent } from 'react';
+
+const Catalogue: VoidFunctionComponent = () => {
+  return <h1>Catalogue</h1>;
+};
+
+export default Catalogue;
---
+++ src/pages/SingIn.tsx
@@ -0,0 +1,7 @@
+import React, { VoidFunctionComponent } from 'react';
+
+const SingIn: VoidFunctionComponent = () => {
+  return <h1>Sing In</h1>;
+};
+
+export default SingIn;
